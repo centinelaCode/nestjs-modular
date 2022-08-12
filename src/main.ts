620a5fff3e97c944c471d5a4e36437d0bc124cd5
centinela1024tb <centinela1024tb@gmail.com>
2022-08-12 12:09:30 -0500

    [DEPLOY] Se cargan archivos para deploy
    => Procfile
    => ajuste de node en package.json

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
-
-  await app.listen(3000);
+  app.enableCors(); // habilita los CORS
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
